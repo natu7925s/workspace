3740dce4ab537a3705bbc4a25a73e0f82d5eb98a
natu7925s <natu7925s@gmail.com>
2024-03-18 16:34:03 +0900

    作業更新

--- Flask/MemoBuddy/app.py
+++ Flask/MemoBuddy/app.py
@@ -1,10 +1,40 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
 
 import secrets
+import datetime
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///memobuddy.db'
+
+db = SQLAlchemy(app)
+dt_jst = datetime.timezone(datetime.timedelta(hours=9), 'JST')
+
+class Memo(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	post_time = db.Column(db.String(32), nullable=False)
+	memo = db.Column(db.String(256), nullable=False)
+
+@app.route("/write", methods=["POST"])
+def write():
+	if request.method == "POST":
+		memo_a = request.form.get("memo")
+		dt_now = datetime.datetime.now(dt_jst).strftime('%Y-%m-%d %H:%M:%S')
+		print(memo_a)
+		print(dt_now)
+		if memo_a != "":	
+			memo = Memo(
+				memo=memo_a,
+				post_time=dt_now
+			)
+			db.session.add(memo)
+			db.session.commit()
+		return redirect("/")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
